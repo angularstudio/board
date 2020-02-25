9cb8f6462f2117ca53453e460631741452c5827a
Matthew Davis <matthew@matthewdavis.io>
2020-02-25 18:23:34 -0500

    bump

--- src/lib/board.component.ts
+++ src/lib/board.component.ts
@@ -1,22 +1,31 @@
-import { Component, Input } from '@angular/core';
-import { Board }            from './board';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Board }                                  from './board';
 
 @Component({
 
     selector: 'board',
-
     template: `
 
         <div class="wrapper"
+             [class.collapsed]="collapsed"
              [style.background-color]="board.backgroundColor"
              [style.width]="board.width">
 
             <div class="inside">
 
+                <div class="collapse-button" (click)="toggle()">
+
+                    <i class="fad fa-bars"></i>
+
+                </div>
+
                 <div class="button-elements">
 
+
                     <board-button *ngFor="let button of board.buttons"
+                                  [collapsed]="collapsed"
                                   [item]="button"></board-button>
+
 
                 </div>
 
@@ -24,6 +33,7 @@ import { Board }            from './board';
                      [style.border-top-color]="board.borderTopColor">
 
                     <studio-button *ngFor="let button of board.bottomButtons"
+                                   [collapsed]="collapsed"
                                    [button]="button"></studio-button>
 
                 </div>
@@ -34,11 +44,22 @@ import { Board }            from './board';
 
     `,
 
-    styleUrls: [ 'board.component.scss' ]
+    styleUrls: [ 'board.component.scss' ],
 
 })
 export class BoardComponent {
 
+    @Input() public collapsed: boolean = true;
     @Input() public board: Board;
+
+    @Output() public onCollapsed: EventEmitter<boolean> = new EventEmitter();
+
+    public toggle(): void {
+
+        this.collapsed = !this.collapsed;
+
+        this.onCollapsed.emit(this.collapsed);
+
+    }
 
 }
